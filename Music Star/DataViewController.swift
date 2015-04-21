5ee3288cb2122fc9ec241d11ca9067ff205e87be
adisha94 <abetawar@gmail.com>
2015-04-20 21:03:53 -0700

    Initial commit
    This uses the new Spotify SDK

--- Music Star/DataViewController.swift
+++ Music Star/DataViewController.swift
@@ -8,25 +8,30 @@
 
 import UIKit
 
-class DataViewController: UIViewController {
+class DataViewController: UIViewController
+{
 
     @IBOutlet weak var dataLabel: UILabel!
     var dataObject: AnyObject?
 
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    override func viewWillAppear(animated: Bool) {
+    override func viewWillAppear(animated: Bool)
+    {
         super.viewWillAppear(animated)
-        if let obj: AnyObject = dataObject {
+        if let obj: AnyObject = dataObject
+        {
             self.dataLabel!.text = obj.description
         } else {
             self.dataLabel!.text = ""
--- Music Star/RootViewController.swift
+++ Music Star/RootViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class RootViewController: UIViewController, UIPageViewControllerDelegate {
+class RootViewController: UIViewController, UIPageViewControllerDelegate
+{
 
     var pageViewController: UIPageViewController?
 
